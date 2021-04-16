f3284d94ea5ff6eb04e856b37ed8d97088463212
root <root@ip-10-0-1-59.us-east-2.compute.internal>
2021-04-16 01:15:26 +0000

    automation of Mapr ticket generation done

--- automation/cleanup_phase.sh
+++ automation/cleanup_phase.sh
@@ -7,12 +7,15 @@ printf "\n\n\n\n\nDELETING SPARK TERRAGEN OPERATOR\n\n\n\n\n"
 kubectl delete sparkapplication.sparkoperator.k8s.io spark-benchmark-terragen -n $namespace
 
 printf "\n\n\n\n\nDELETING SPARK TERRASORT OPERATOR\n\n\n\n\n"
-# kubectl delete sparkapplication.sparkoperator.k8s.io spark-benchmark-terrasort -n $namespace
+kubectl delete sparkapplication.sparkoperator.k8s.io spark-benchmark-terrasort -n $namespace
 
 printf "\n\n\n\n\nDELETING TEMP POD\n\n\n\n\n"
 kubectl delete pod spark-benchmark-temp-pod -n $namespace
 
 printf "\n\n\n\n\nDELETING PVC\n\n\n\n\n"
 kubectl delete pvc spark-benchmark-claim -n $namespace
+
+printf "\n\n\n\n\nDELETING MAPR TICKET\n\n\n\n\n"
+kubectl delete secret jenkins-secret -n $namespace
 
 printf "\n\n\n\n\nDone .... :)\n\n\n\n\n"
