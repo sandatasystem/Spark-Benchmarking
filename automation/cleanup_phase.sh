c8385077570da0b369930b909ed177074f8406f0
Varun Rao <vrao@sandatasystem.com>
2021-07-13 20:44:21 -0700

    Formatting logging so displays are easy to understand

--- automation/cleanup_phase.sh
+++ automation/cleanup_phase.sh
@@ -1,22 +1,24 @@
 namespace=$1
 
-printf "\n\nDELETING SPARK JOIN OPERATOR\n\n"
-kubectl delete sparkapplication spark-benchmark-join -n $namespace --ignore-not-found=true
+printf "\n\n\n\n\nSUBMITTING CLEANUP PHASE \n\n\n\n\n"
 
-printf "\n\nDELETING SPARK TERAGEN OPERATOR\n\n"
-kubectl delete sparkapplication spark-benchmark-teragen -n $namespace --ignore-not-found=true
+printf "\n\n\tDELETING SPARK JOIN OPERATOR\n\n"
+kubectl delete sparkapplication spark-benchmark-join -n $namespace --ignore-not-found=true>/dev/null
 
-printf "\n\nDELETING SPARK TERSORT OPERATOR\n\n"
-kubectl delete sparkapplication spark-benchmark-terasort -n $namespace --ignore-not-found=true
+printf "\n\n\tDELETING SPARK TERAGEN OPERATOR\n\n"
+kubectl delete sparkapplication spark-benchmark-teragen -n $namespace --ignore-not-found=true>/dev/null
 
-printf "\n\nDELETING SPARK TERAVALIDATE OPERATOR\n\n"
-kubectl delete sparkapplication spark-benchmark-teravalidate -n $namespace --ignore-not-found=true
+printf "\n\n\tDELETING SPARK TERSORT OPERATOR\n\n"
+kubectl delete sparkapplication spark-benchmark-terasort -n $namespace --ignore-not-found=true>/dev/null
 
-printf "\n\nDELETING TEMP POD\n\n"
-kubectl delete pod spark-benchmark-temp-pod -n $namespace --ignore-not-found=true
+printf "\n\n\tDELETING SPARK TERAVALIDATE OPERATOR\n\n"
+kubectl delete sparkapplication spark-benchmark-teravalidate -n $namespace --ignore-not-found=true>/dev/null
 
-printf "\n\nDELETING PVC\n\n"
-kubectl delete pvc spark-benchmark-claim -n $namespace --ignore-not-found=true
+printf "\n\n\tDELETING TEMP POD\n\n"
+kubectl delete pod spark-benchmark-temp-pod -n $namespace --ignore-not-found=true>/dev/null
 
-printf "\n\nDELETING MAPR TICKET\n\n"
-kubectl delete secret jenkins-secret -n $namespace --ignore-not-found=true
+printf "\n\n\tDELETING PVC\n\n"
+kubectl delete pvc spark-benchmark-claim -n $namespace --ignore-not-found=true>/dev/null
+
+printf "\n\n\tDELETING MAPR TICKET\n\n"
+kubectl delete secret jenkins-secret -n $namespace --ignore-not-found=true>/dev/null
